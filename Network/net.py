fbed13621adb87536ead7456c56ee26bfe18da3e
Stepan <stepan.sido@docsie.io>
2020-10-03 19:02:23 +0400

    Added new sounds, added Net

--- Network/net.py
+++ Network/net.py
@@ -5,7 +5,7 @@ import torch.optim as optim
 
 
 class Network(nn.Module):
-    def __init__(self, net_plane: list,activate: str = "ReLU", reduction: str = "mean", optimizer_momentum: float = 0.9,optimizer_lr: float = 0.01):
+    def __init__(self, net_plane: list,activate: str = "Sigmoid", reduction: str = "mean", optimizer_momentum: float = 0.9,optimizer_lr: float = 0.01):
         super(Network,self).__init__()
         self.criterion = nn.MSELoss(reduction=reduction)
         if activate == "ReLU":
@@ -14,6 +14,16 @@ class Network(nn.Module):
             self.activate = nn.Sigmoid()
         elif activate == "Tanh":
             self.activate = nn.Tanh()
+        elif activate == "LeakyReLU":
+            self.activate = nn.LeakyReLU()
+        elif activate == "RReLU":
+            self.activate = self.activate = nn.RReLU()
+        elif activate == "Hardsigmoid":
+            self.activate = self.activate = nn.Hardsigmoid()
+        elif activate == "Hardtanh":
+            self.activate = self.activate = nn.Hardtanh()
+        elif activate == "Hardswish":
+            self.activate = self.activate = nn.Hardswish()
         self.layers = nn.Sequential()
         plane_index = 0
         layer_index = 0
@@ -28,7 +38,7 @@ class Network(nn.Module):
         """Values from training"""
         self.loss = None
         self.output_from_net = None
-        self.optimizer = optim.SGD(params=self.parameters(),lr=optimizer_lr, momentum=optimizer_momentum)
+        self.optimizer = optim.SGD(params=self.parameters(),lr=optimizer_lr)
 
     def forward(self, inputs):
         return self.layers(inputs)
@@ -45,3 +55,83 @@ class Network(nn.Module):
     def load(self, path: str):
         self.layers = tc.load(path)
 
+
+"""from tqdm import tqdm
+import numpy as np
+
+import torch
+from torch import nn
+from torch import tensor
+from torch import optim
+
+import matplotlib.pyplot as plt
+
+torch.manual_seed(0)
+device = 'cpu'
+
+# XOR gate inputs and outputs.
+X = xor_input = tensor([[0,0], [0,1], [1,0], [1,1]]).float().to(device)
+Y = xor_output = tensor([[0],[1],[1],[0]]).float().to(device)
+
+
+# Use tensor.shape to get the shape of the matrix/tensor.
+num_data, input_dim = X.shape
+print('Inputs Dim:', input_dim) # i.e. n=2 
+
+num_data, output_dim = Y.shape
+print('Output Dim:', output_dim) 
+print('No. of Data:', num_data) # i.e. n=4
+
+# Step 1: Initialization. 
+
+# Initialize the model.
+# Set the hidden dimension size.
+hidden_dim = 5
+# Use Sequential to define a simple feed-forward network.
+model = nn.Sequential(
+            # Use nn.Linear to get our simple perceptron.
+            nn.Linear(input_dim, hidden_dim),
+            # Use nn.Sigmoid to get our sigmoid non-linearity.
+            nn.Sigmoid(),
+            # Second layer neurons.
+            nn.Linear(hidden_dim, output_dim),
+            nn.Sigmoid()
+        )
+model
+
+# Initialize the optimizer
+learning_rate = 0.3
+optimizer = optim.SGD(model.parameters(), lr=learning_rate)
+
+# Initialize the loss function.
+criterion = nn.MSELoss()
+
+# Initialize the stopping criteria
+# For simplicity, just stop training after certain no. of epochs.
+num_epochs = 5000 
+
+losses = [] # Keeps track of the loses.
+
+# Step 2-4 of training routine.
+
+for _e in tqdm(range(num_epochs)):
+    # Reset the gradient after every epoch. 
+    optimizer.zero_grad() 
+    # Step 2: Foward Propagation
+    predictions = model(X)
+
+    # Step 3: Back Propagation 
+    # Calculate the cost between the predictions and the truth.
+    loss = criterion(predictions, Y)
+    # Remember to back propagate the loss you've computed above.
+    loss.backward()
+
+    # Step 4: Optimizer take a step and update the weights.
+    optimizer.step()
+
+    # Log the loss value as we proceed through the epochs.
+    losses.append(loss.data.item())
+
+
+plt.plot(losses)
+plt.show()"""
--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import librosa
+import scipy
+from scipy import stats
 from matplotlib import pyplot as plt
 from os import listdir
 from Network.net import Network
 import torch
 import statistics
 import librosa.display
+import numpy as np
 
-
-def split_sound(sound,count_split: int = 1000):
+def split_sound(sound,count_split: int = 3000):
     splited = []
     for splited_index in range(len(sound)//count_split):
         splited.append(sound[:count_split])
         sound = sound[count_split:]
-    return splited
+    return splited[:len(splited)-1]
 def get_center_sound(splited_sound: list, count_split: int = 10000):
     if len(splited_sound) < count_split+count_split+1:#if len sound is too small
         return False
@@ -24,9 +26,69 @@ def get_center_sound(splited_sound: list, count_split: int = 10000):
 def filter_sound(sound):
     return split_sound(get_center_sound(sound))
 
+def make_result(result: list):
+    if result[0] > result[1]:
+        return "hmm"
+    else:
+        return "firk"
+def get_base_features(wav_path):
+    ff_list = []
 
-firks = ["./sounds/firk/"+i for i in listdir("./sounds/firk")]
-hmms = ["./sounds/hmm/"+i for i in listdir("./sounds/hmm")]
+    y, sr = librosa.load(wav_path, sr=None)
+
+    y_harmonic, y_percussive = librosa.effects.hpss(y)
+
+    tempo, beat_frames = librosa.beat.beat_track(y=y_harmonic, sr=sr)
+    chroma = librosa.feature.chroma_cens(y=y_harmonic, sr=sr)
+    mfccs = librosa.feature.mfcc(y=y_harmonic, sr=sr, n_mfcc=13)
+    cent = librosa.feature.spectral_centroid(y=y, sr=sr)
+    contrast = librosa.feature.spectral_contrast(y=y_harmonic, sr=sr)
+    rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr)
+    zrate = librosa.feature.zero_crossing_rate(y_harmonic)
+
+    chroma_mean = np.mean(chroma, axis=1)
+    chroma_std = np.std(chroma, axis=1)
+
+    for i in range(0, 12):
+        ff_list.append(chroma_mean[i])
+    for i in range(0, 12):
+        ff_list.append(chroma_std[i])
+
+    mfccs_mean = np.mean(mfccs, axis=1)
+    mfccs_std = np.std(mfccs, axis=1)
+
+    for i in range(0, 13):
+        ff_list.append(mfccs_mean[i])
+    for i in range(0, 13):
+        ff_list.append(mfccs_std[i])
+
+    cent_mean = np.mean(cent)
+    cent_std = np.std(cent)
+    cent_skew = scipy.stats.skew(cent, axis=1)[0]
+
+    contrast_mean = np.mean(contrast, axis=1)
+    contrast_std = np.std(contrast, axis=1)
+
+    rolloff_mean = np.mean(rolloff)
+    rolloff_std = np.std(rolloff)
+
+    data = np.concatenate(([cent_mean, cent_std, cent_skew],
+                           contrast_mean, contrast_std,
+                           [rolloff_mean, rolloff_std, rolloff_std]), axis=0)
+    ff_list += list(data)
+
+    zrate_mean = np.mean(zrate)
+    zrate_std = np.std(zrate)
+    zrate_skew = scipy.stats.skew(zrate, axis=1)[0]
+
+    ff_list += [zrate_mean, zrate_std, zrate_skew]
+
+    ff_list.append(tempo)
+
+    return ff_list
+
+firks = ["./sounds/train_sounds/firk/" + i for i in listdir("sounds/train_sounds/firk")]
+hmms = ["./sounds/train_sounds/hmm/" + i for i in listdir("sounds/train_sounds/hmm")]
 
 
 
@@ -39,92 +101,67 @@ for path_to_sound in range(len(firks)+len(hmms)):
         training_sounds.append({
             "name":"firk",
             "path": firks[path_to_sound],
-            "must_output":[0.1]
+            "must_output":[0.0, 1.0]
         })
         training_sounds.append({
             "name":"hmm",
             "path": hmms[path_to_sound],
-            "must_output": [0.9]
+            "must_output": [1.0, 0.0]
         })
     except IndexError:
         continue
 
 """This is diagrams"""
 # for snd in training_sounds:
-#     print()
 #     sound = librosa.load(snd["path"])[0]
-#     X = librosa.stft(sound)
+#     X = librosa.stft(np.array(get_center_sound(sound)))
 #     Xdb = librosa.amplitude_to_db(abs(X))
-#     plt.figure(figsize=(14, 5))
-#     librosa.display.specshow(Xdb, x_axis='time', y_axis='hz')
-#     plt.xlabel(snd["name"], fontsize=12, color='black')
+#     plt.figure()
+#     librosa.display.specshow(Xdb, x_axis='time', y_axis='log')
 #     plt.colorbar()
+#
+#     plt.xlabel(snd["name"], fontsize=12, color='black')
 #     plt.show()
 
+
+
 errors_Net_create_30 = []
-Net_create_30 = Network([1000,2000,1000,500,200,100,1],activate="Sigmoid",optimizer_lr=0.01 ,optimizer_momentum=0.9)
-
-
-for i in range(1):
+Net_create_30 = Network([6150,1300,5000,3000,1000,2],activate="Sigmoid",optimizer_lr=0.01)
+# Net_create_30.load(path="10.0.0")
+# for snd_now in split_sound(librosa.load(firks[0])[0]):
+#     stft_sound = librosa.stft(np.array(snd_now))
+#     temperature = librosa.amplitude_to_db(abs(stft_sound))
+#     temperature = temperature.reshape(len(temperature)*len(temperature[0]))
+#     output = Net_create_30.forward(inputs=torch.FloatTensor(temperature))
+#     errors_Net_create_30.append(Net_create_30.loss)
+#     print(output)
+for i in range(40):
     for sounds_dict in training_sounds:
-        sound = librosa.load(sounds_dict["path"])[0]
-        sounds = filter_sound(sound)
-
-        if sounds != False:#if this not False
-            outputs = []
-            for splited_sound in sounds:
-                print(statistics.mean(splited_sound))
-                output = Net_create_30.training_net(inputs=torch.FloatTensor(splited_sound), must_outputs=torch.FloatTensor(sounds_dict["must_output"]))
-                errors_Net_create_30.append(Net_create_30.loss)
-
-
-
+        for snd_now in split_sound(librosa.load(sounds_dict["path"])[0]):
+            stft_sound = librosa.stft(np.array(snd_now))
+            temperature = librosa.amplitude_to_db(abs(stft_sound))
+            temperature = temperature.reshape(len(temperature)*len(temperature[0]))
+            output = Net_create_30.training_net(inputs=torch.FloatTensor(temperature), must_outputs=torch.FloatTensor(sounds_dict["must_output"]))
+            errors_Net_create_30.append(Net_create_30.loss)
+            print(output)
+Net_create_30.save("2_10.0.0_40")
 plt.plot(errors_Net_create_30)
 plt.show()
-print("--------------------------")
-for i in range(5):
-    sound = librosa.load(hmms[i])[0]
-    sounds = filter_sound(sound)
-    if sounds != False:#if this not False
-        all_out = []
-        for splited_sound in sounds:
-            plt.plot(splited_sound)
-            output = Net_create_30.forward(inputs=torch.FloatTensor(splited_sound))
-            all_out.append(output.detach().numpy()[0])
-        print(statistics.mean(all_out))
-        plt.show()
-print("----------firk----------")
-for i in range(5):
-    sound = librosa.load(firks[i])[0]
-    sounds = filter_sound(sound)
 
-    if sounds != False:#if this not False
-        all_out = []
-        for splited_sound in sounds:
-            plt.plot(splited_sound)
-            output = Net_create_30.forward(inputs=torch.FloatTensor(splited_sound))
-            all_out.append(output.detach().numpy()[0])
-        print(statistics.mean(all_out))
-        plt.show()
-"""
-#######НЕЙРОСЕТЬ#######
-Одна нейросеть берет 30 кусочков звуков, разделенных функцией split_sound, с параметром 500.
-Обучается отвечать что это именно те звуки которые, обозначены
-
-###которая принимает каждый раз новых 30 кусочков###
-
-
-D-> O-> O\
-D-> O-> O-> o-> 1 (Hmm)
-D-> O-> O-> o-> 1 (Firk)
-D-> O-> O/
-
-
-
-Вторая которая принимает ответы первой, 30 ответов, и делает вывод, какой был звук
-
-D-> O-> O\
-D-> O-> O-> o-> 1 (Hmm)
-D-> O-> O-> o-> 1 (Firk)
-D-> O-> O/
-"""
+# for i in range(1):
+#     for sounds_dict in training_sounds:
+#         print("--------------------------")
+#         print(sounds_dict["name"])
+#         output = Net_create_30.forward(inputs=torch.FloatTensor(get_base_features(sounds_dict["path"])))
+#         print(output)
+#         print("=============================")
+#
+# print("+++++++++++++++++++++++++++++++++++++++")
+# print("firk:")
+# output = Net_create_30.forward(inputs=torch.FloatTensor(get_base_features(
+#     "./sounds/test_sounds/firk/3e6fb03cf985866dc9f9f7070e2de3962a9125870fb9230c58297e4b68869332.wav")))
+# print(output)
+# print("hmm:")
+# output = Net_create_30.forward(inputs=torch.FloatTensor(get_base_features(
+#     "./sounds/test_sounds/hmm/2b30b03245cf21cfc202aeee47be752ad51b350761b7381ddb3c1def86adc5cf.wav")))
+# print(output)
