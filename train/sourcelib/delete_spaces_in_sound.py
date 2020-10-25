943ed2c2e373c9b67f0b417ba8bf65e783904285
Stepan <stepan.sido@docsie.io>
2020-10-25 15:44:59 +0400

    Update lib

--- train/sourcelib/delete_spaces_in_sound.py
+++ train/sourcelib/delete_spaces_in_sound.py
@@ -7,10 +7,13 @@ after run this file.
 
 
 import librosa
-from os import listdir
+from os import listdir, mkdir
 from numpy import delete, flip
 from scipy.io.wavfile import write as write_wav
-sound_pathes = [i for i in listdir("/") if i[len(i) - 4:] == ".wav"]
+sound_pathes = [i for i in listdir() if i[len(i) - 4:] == ".wav"]
+if not "new_sounds" in listdir():
+	mkdir("new_sounds")
+print(sound_pathes)
 
 for sound in sound_pathes:
     snd,sr = librosa.load(sound)
@@ -30,4 +33,4 @@ for sound in sound_pathes:
         snd = delete(snd,index)
 
 
-    write_wav(sound+"_new.wav", sr, snd)
+    write_wav("new_sounds/"+sound, sr, snd)
